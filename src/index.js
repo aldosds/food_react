02c443055c91a39030f0623a27f0e762e071a35b
Aldo Santos <aldoacesso@gmail.com>
2024-02-13 09:00:38 -0300

    Created the pages, checkout, history and created the routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./style/global.css"
-import Home from './pages/home/home';
+import Rotas from './rotas.js';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <Home/>
+    <Rotas/>
   </>
 );
