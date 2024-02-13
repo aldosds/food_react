2155408e7d9547d602aaa521e3230ad8536a9890
Aldo Santos <aldoacesso@gmail.com>
2024-02-13 07:29:30 -0300

    Created navbar on home

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,17 @@
+import Navbar from "../../components/navbar/navebar";
+
 function Home() {
     return <>
-        
+        <Navbar/>
+
+        <div className="container">
+            <div className="titulo text-center">
+                <h1>Nosso Cardápio</h1>
+                <p className="subtitulo">
+                Clique em adicionar para colocar os produtos na sacola de compras. Se preferir, você pode pedir pelo WhatsApp: (11) 94785-2140
+                </p>
+            </div>
+        </div>
     </>
 }
 
